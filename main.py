287d2cbd6bf8bf07371a817179cac51767724e6c
syyxsxx <wsy@8glabs.com>
2023-12-21 22:49:12 +0800

    add dist train

---
+++ main.py
@@ -0,0 +1,55 @@
+import argparse
+import warnings
+
+from magvit2_pytorch import VideoTokenizer, VideoTokenizerTrainer
+
+parser = argparse.ArgumentParser(description='Magvit Training')
+parser.add_argument('data', metavar='DIR', nargs='?', default='imagenet',
+                    help='path to dataset (default: imagenet)')
+parser.add_argument('--data-type', default='videos', type=str,
+                    help='dataset type')
+
+parser.add_argument('--bn', default=1, type=int,
+                    help='batchsize')
+
+
+def main():
+    args = parser.parse_args()
+    tokenizer = VideoTokenizer(
+        image_size = 128,
+        init_dim = 64,
+        max_dim = 512,
+        codebook_size = 1024,
+        layers = (
+            'residual',
+            'compress_space',
+            ('consecutive_residual', 2),
+            'compress_space',
+            ('consecutive_residual', 2),
+            'linear_attend_space',
+            'compress_space',
+            ('consecutive_residual', 2),
+            'attend_space',
+            'compress_time',
+            ('consecutive_residual', 2),
+            'compress_time',
+            ('consecutive_residual', 2),
+            'attend_time',
+        )
+    )
+
+    trainer = VideoTokenizerTrainer(
+        tokenizer,
+        dataset_folder = args.data,     # folder of either videos or images, depending on setting below
+        dataset_type = args.data_type,                        # 'videos' or 'images', prior papers have shown pretraining on images to be effective for video synthesis
+        batch_size = args.bn,
+        grad_accum_every = 8,
+        learning_rate = 2e-5,
+        num_train_steps = 1_000_000
+    )
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
+
